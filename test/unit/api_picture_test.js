c7c68c9b62a93a6d2e1ee333d16239d468fd3912
hyeri0609 <hyeri_0609@protonmail.com>
2017-11-22 01:32:42 +0000

    Update test for picture api

--- test/unit/api_picture_test.js
+++ test/unit/api_picture_test.js
@@ -1,34 +1,37 @@
 'use strict';
-
-var express = require('express');
-var favicon = require('serve-favicon');
 var request = require('supertest');
 var sinon = require('sinon');
 var should = require('should');
 
+var express = require('express');
+var favicon = require('serve-favicon');
+var bodyParser = require('body-parser');
+
 describe('picturejs api', function() {
     var app;
     
-    beforeEach(function() {
+    before(function() {
         app = express();
-        app.set('port', (process.env.PORT || 8080));
-        app.set('views', __dirname + '/views');
+        //app.set('port', (process.env.PORT || 8080));
+        app.set('views', './views');
         app.set('view engine', 'ejs');
 
-        app.use(express.static(__dirname + '/public'));
-        app.use(favicon(__dirname + '../../public/icons/favicon.ico'));
+        app.use(express.static('./public'));
+        app.use(favicon('./public/icons/favicon.ico'));
         app.use(bodyParser.urlencoded({ extended: true }));
         
         var apirouter = express.Router();
-            apirouter.get('/', function(req, res) {
+        apirouter.get('/', function(req, res) {
             res.json({ message: 'ok' });   
         });
-        var picture = require('./api/picture');
+        var picture = require('../../api/picture');
         picture(apirouter);
+        
+        app.use('/api', apirouter);
     });
     
     describe('start express and add picture.js api', function() {
-        it('should run at localhost:8080', function(done) {
+        it('should response from /', function(done) {
             request(app.listen())
                 .get('/')
                 .end(function() {
